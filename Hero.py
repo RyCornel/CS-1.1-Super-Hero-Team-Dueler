cb4b09e1ea17b1cb4909dadb0f2b81de82b43eba
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-11-23 02:02:32 -0800

    Fight Method Created

--- Hero.py
+++ Hero.py
@@ -1,9 +1,17 @@
+from random import random
+
 class Hero:
     def __init__(self, name, starting_health = 100):
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
-    def fight(self, opponent):
-        self.opponent = Hero
-
     
+    def fight(self, opponent):
+        self.opponent = opponent
+        
+
+
+if __name__ == "__main__":
+    my_hero = Hero("Grace Hopper", 200)
+    print(my_hero.name)
+    print(my_hero.current_health)
