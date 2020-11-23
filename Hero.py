5ba6566fd02701074ec56c65656603339b0bcf13
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-11-23 09:40:32 -0800

    Update Hero.py

--- Hero.py
+++ Hero.py
@@ -1,4 +1,4 @@
-from random import random
+import random
 
 class Hero:
     def __init__(self, name, starting_health = 100):
@@ -8,10 +8,20 @@ class Hero:
     
     def fight(self, opponent):
         self.opponent = opponent
-        
+        hero1 = self
+        hero2 = opponent
+        fighters = (hero1, hero2)
+        print(random.choice(fighters))
 
 
 if __name__ == "__main__":
     my_hero = Hero("Grace Hopper", 200)
     print(my_hero.name)
     print(my_hero.current_health)
+
+if __name__ == "__main__":
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+
+    hero1.fight(hero2)
+
