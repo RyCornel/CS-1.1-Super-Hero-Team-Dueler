2022c7b68c3b98faf174fa0747baee003d952066
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-11-22 20:50:29 -0800

    My_Dogs.py Created Commit

--- Dog.py
+++ Dog.py
@@ -1,9 +1,18 @@
 class Dog:
-    def __init__(self, name):
+    def __init__(self, name, breed):
         self.name = name
+        self.breed = breed
         print("dog initialized!")
 
-my_dog = Dog("Rex")
+    def bark(self):
+        print("Woof!")
+
+my_dog = Dog("Rex", "SuperDog")
 print(my_dog)
 print(my_dog.name)
+print(my_dog.breed)
+print(my_dog.bark())
+#my_dog.breed = "SuperDog"
+#print(my_dog.breed)
+
 
