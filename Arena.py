01a2ae0e2a827ed021a85ccf5652d994de809d4b
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-11-23 20:45:20 -0800

    Create_Hero Method Complete

--- Arena.py
+++ Arena.py
@@ -27,3 +27,18 @@ class Arena:
         max_strength = input("What is the max strength of the armor?")
 
         return Armor(name, max_strength)
+
+    def create_hero(self):
+        hero_name = input("Hero's name: ")
+        hero = Hero(hero_name)
+        add_item = None
+        while add_item != "4":
+            add_item = input("[1] Add ability\n[2] Add weapon\n[3] Add armor\n[4] Done adding items\n\nYour choice: ")
+            if add_item == "1":
+                hero.add_ability(self.create_ability())
+            elif add_item == "2":
+                hero.add_weapon(self.create_weapon())
+            elif add_item == "3":
+                hero.add_armor(self.create_armor())
+
+        return hero
