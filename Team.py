66e71ac44f2a2f68777cc3b45389f24018587322
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-11-23 18:15:34 -0800

    Update Team.py

--- Team.py
+++ Team.py
@@ -4,5 +4,24 @@ class Team:
     def __init__(self, name):
         self.name = name
         self.heroes = list()
+
+    def remove_hero(self, name):
+        foundHero = False
+
+        for Hero in self.heroes:
+            if Hero.name == name:
+                self.heroes.remove(Hero)
+                foundHero = True
+
+        if not foundHero:
+            return 0
+
+    def view_all_heroes(self):
+        print([Hero for Hero in self.heroes])
+
+    def add_hero(self, Hero):
+        self.heroes.append(Hero)
+
+    
         
     
