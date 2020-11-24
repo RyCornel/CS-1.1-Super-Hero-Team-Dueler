c5b9aeb35c30e95522e681f64a9de8ec567b51df
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-11-23 17:36:40 -0800

    Team Class Created

---
+++ Team.py
@@ -0,0 +1,7 @@
+from Hero import Hero
+
+class Team:
+    def __init__(self, add_hero, remove_hero, view_all_heroes)
+        self.add_hero = add_hero
+        self.remove_hero = remove_hero
+        self.view_all_heroes = view_all_heroes
