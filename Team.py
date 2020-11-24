2454e602f65bdcd93d8a81e7ba019da3277168e2
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-11-23 19:18:38 -0800

    Attack Method Update

--- Team.py
+++ Team.py
@@ -1,4 +1,5 @@
 from Hero import Hero
+import random
 
 class Team:
     def __init__(self, name):
@@ -30,5 +31,27 @@ class Team:
     def revive_heroes(self, health = 100):
         for Hero in self.heroes:
             Hero.current_health = health
+
+    def attack(self, other_team):
+
+        living_heores = list()
+        living_opponents = list()
+
+        for Hero in self.heroes:
+            living_heores.append(Hero)
+
+        for Hero in other_team.heroes:
+            living_opponents.append(Hero)
+
+        while len(living_heores) > 0 and len(living_opponents) > 0:
         
+            hero1 = random.choice(living_heores)
+            hero2 = random.choice(living_opponents)
+            
+            hero1.fight(hero2)
+
+            if hero1.is_alive():
+                living_opponents.remove(hero2)
+            else:
+                living_heores.remove(hero1)
     
