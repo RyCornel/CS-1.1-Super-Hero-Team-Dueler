9b706479a01f1240b21dc2014bb48067bc7582a7
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-11-23 17:40:18 -0800

    Team.py Update

--- Team.py
+++ Team.py
@@ -1,7 +1,8 @@
 from Hero import Hero
 
 class Team:
-    def __init__(self, add_hero, remove_hero, view_all_heroes)
-        self.add_hero = add_hero
-        self.remove_hero = remove_hero
-        self.view_all_heroes = view_all_heroes
+    def __init__(self, name):
+        self.name = name
+        self.heroes = list()
+        
+    
